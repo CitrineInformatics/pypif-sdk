19e9a401adfe624a79b376060de413512a8501b3
Max Hutchinson <maxhutch@gmail.com>
2017-09-12 14:56:13 -0500

    Adding read view and admin files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pypif-sdk',
+      version='1.3.0',
+      url='http://github.com/CitrineInformatics/pypif-sdk',
+      description='High level support for working with Physical Information Files (PIF)',
+      author='Max Hutchinson',
+      author_email='maxhutch@citrine.io',
+      packages=find_packages(),
+      install_requires=[
+          'pypif'
+      ])
