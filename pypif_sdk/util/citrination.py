c62256c41364ca72e41e0ee8c22c6907028439c1
Max Hutchinson <maxhutch@gmail.com>
2017-09-14 11:23:10 -0500

    Pull default site from "CITRINATION_SITE" (with good default)

--- pypif_sdk/util/citrination.py
+++ pypif_sdk/util/citrination.py
@@ -2,9 +2,11 @@ from os import environ
 from citrination_client import CitrinationClient
 
 
-def get_client(site='https://citrination.com'):
+def get_client(site=None):
     """Get a citrination client"""
     if 'CITRINATION_API_KEY' not in environ:
         return None
+    if not site:
+        site = environ.get("CITRINATION_SITE", "https://citrination.com")
     return CitrinationClient(environ['CITRINATION_API_KEY'], site)
 
