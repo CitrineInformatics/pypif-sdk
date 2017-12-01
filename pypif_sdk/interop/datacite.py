6af21d6fde4fa7a0ecf55b9edd7f450972d4208f
Max Hutchinson <maxhutch@gmail.com>
2017-11-30 19:14:20 -0600

    Docs

--- pypif_sdk/interop/datacite.py
+++ pypif_sdk/interop/datacite.py
@@ -3,6 +3,12 @@ from ..func.update_funcs import merge
 
 
 def parse_name_string(full_name):
+    """
+    Parse a full name into a Name object
+
+    :param full_name: e.g. "John Smith" or "Smith, John"
+    :return: Name object
+    """
     name = Name()
     if "," in full_name:
         toks = full_name.split(",")
@@ -16,6 +22,11 @@ def parse_name_string(full_name):
 
 
 def creator_to_person(creator):
+    """
+    Parse the creator block in datacite into a Person
+    :param creator: block in datacite format
+    :return: Person
+    """
     name = Name()
     if "creatorName" in creator:
         name = parse_name_string(creator["creatorName"])
@@ -29,6 +40,11 @@ def creator_to_person(creator):
 
 
 def datacite_to_pif_reference(dc):
+    """
+    Parse a top-level datacite dictionary into a Reference
+    :param dc: dictionary containing datacite metadata
+    :return: Reference corresponding to that datacite entry
+    """
     ref = Reference()
     if dc.get('identifier').get('identifierType') == "DOI":
         ref.doi = dc.get('identifier').get('identifier')
@@ -42,11 +58,22 @@ def datacite_to_pif_reference(dc):
 
 
 def datacite_to_pif(dc):
+    """
+    Parse a top-level datacite dictionary into a PIF
+    :param dc: dictionary containing datacite metadata
+    :return: System containing metadata from datacite
+    """
     system = System()
     system.references = [datacite_to_pif_reference(dc)]
     return system
 
 
 def add_datacite(pif, dc):
+    """
+    Add datacite metadata to an existing pif (out-of-place)
+    :param pif: to which metadata should be added
+    :param dc: dictionary with datacite metadata
+    :return: new PIF with metadata added
+    """
     meta_pif = datacite_to_pif(dc)
     return merge(pif, meta_pif)
