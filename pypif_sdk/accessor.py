decc4a425c82f8da35027cd859ce4161b0284ac2
Jo Hill <joanneljhill@gmail.com>
2018-02-15 11:16:57 -0800

    Adding backward compatibility

--- pypif_sdk/accessor.py
+++ pypif_sdk/accessor.py
@@ -1,4 +1,9 @@
+from warnings import warn
 
+def get_propety_by_name(pif, name):
+    """Get a property by name"""
+    warn("This method has been deprecated in favor of get_property_by_name")
+    return next((x for x in pif.properties if x.name == name), None)
 
 def get_property_by_name(pif, name):
     """Get a property by name"""
